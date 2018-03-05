0a38904e1e42000e5463f8c45e55fa32cae7b6d4
lying <15717302379@163.com>
2018-03-05 10:40:38 +0800

    test

---
+++ example-rabbitmq/src/test/java/com/lying/examplerabbitmq/ExampleRabbitmqApplicationTests.java
@@ -0,0 +1,21 @@
+package com.lying.examplerabbitmq;
+
+import com.lying.examplerabbitmq.rabbitmq.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleRabbitmqApplicationTests {
+
+	@Autowired
+	private Sender sender;
+
+	@Test
+	public void hello() throws Exception {
+		sender.send();
+	}
+}
