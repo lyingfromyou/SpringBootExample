48ec8ac08579d9319e3131b64b8c30e122c6d88f
lying <15717302379@163.com>
2018-03-12 16:51:26 +0800

    restful

---
+++ example-restful/src/main/java/com/lying/examplerestful/ExampleRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.lying.examplerestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleRestfulApplication.class, args);
+	}
+}
